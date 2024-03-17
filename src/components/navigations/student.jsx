8294442bafae99c58e35d66ae30aafeff465df82
enhasya <enhasya@outlook.co.id>
2024-03-18 06:29:08 +0700

    update

--- src/components/navigations/student.jsx
+++ src/components/navigations/student.jsx
@@ -15,17 +15,24 @@ const NavigationStudent = () => {
         Student Page
       </div>
       <div className="flex flex-row gap-4 items-center justify-end w-full lg:w-auto">
-        <button className="bg-[#FFFFFF]/5 transition-all hover:bg-[#FFFFFF]/25 px-4 py-2 rounded-lg flex flex-row gap-4 items-center justify-center w-auto h-auto mt-0">
-          <p className="text-[#FFFFFF] font-medium text-xs tracking-tight">
-            Pengaturan Akun
-          </p>
-          <Image src={icon.accountManagement} width="14" height="14" />
-        </button>
+        <div className="relative">
+          <button className="bg-[#FFFFFF]/5 transition-all hover:bg-[#FFFFFF]/25 px-4 py-2 rounded-lg flex flex-row gap-4 items-center justify-start w-auto lg:w-[360px] h-auto mt-0">
+            <Image src={icon.accountManagement} width="16" height="16" />{" "}
+            <p className="text-[#FFFFFF] font-medium text-sm tracking-tight truncate">
+              Nama Pengguna
+            </p>
+          </button>
+          <div className="fixed w-full lg:w-[240px] top-20 bg-[#FFFFFF] p-4 rounded-lg shadow-none lg:shadow-2xl lg:shadow-[#292D32]/10">
+            <p className="text-[#2563EB] font-medium text-xs tracking-tight">
+              Nama Pengguna
+            </p>
+          </div>
+        </div>
         <button className="bg-[#FFFFFF] transition-all hover:bg-[#FFFFFF]/80 px-4 py-2 rounded-lg flex flex-row gap-4 items-center justify-center w-auto h-auto mt-0">
-          <p className="text-[#2563EB] font-medium text-xs tracking-tight">
+          <p className="text-[#2563EB] font-medium text-sm tracking-tight">
             Keluar
           </p>
-          <Image src={icon.signOut} width="14" height="14" />
+          <Image src={icon.signOut} width="16" height="16" />
         </button>
       </div>
     </div>
