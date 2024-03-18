aef822e324f811321e698638f72624b292d0280e
enhasya <enhasya@outlook.co.id>
2024-03-18 23:08:26 +0700

    update

--- src/components/navigations/student.jsx
+++ src/components/navigations/student.jsx
@@ -15,17 +15,40 @@ const NavigationStudent = () => {
         Student Page
       </p>
       <div className="flex flex-row lg:flex-col gap-4 items-center justify-end w-full">
-        <button className="bg-[#FFFFFF]/5 transition-all hover:bg-[#FFFFFF]/25 px-4 py-4 rounded-lg flex flex-col gap-2 items-start justify-center w-full h-auto mt-0">
-          <div className="flex flex-row gap-2 items-center w-full h-auto">
-            <Image src={icon.accountManagement} width="16" height="16" />{" "}
-            <p className="text-[#FFFFFF] font-medium text-sm tracking-tight truncate">
-              Nama Pengguna
+        <div className="bg-[#FFFFFF]/5 transition-all hover:bg-[#FFFFFF]/25 px-4 py-4 rounded-lg flex flex-col gap-4 items-start justify-center w-full h-auto mt-0">
+          <div className="group flex flex-col gap-2 items-start justify-center w-full h-auto">
+            <div className="flex flex-row gap-2 items-center justify-between w-full h-auto">
+              <p className="text-[#FFFFFF] font-medium text-sm tracking-tight truncate">
+                Nama Pengguna
+              </p>
+              <Image
+                className="group opacity-0 group-hover:opacity-100"
+                src={icon.accountManagement}
+                width="16"
+                height="16"
+              />{" "}
+            </div>
+            <p className="text-[#FFFFFF] font-normal text-xs tracking-tight truncate">
+              Muhammad Naufal R.
             </p>
           </div>
-          <p className="hidden lg:flex text-[#FFFFFF] font-medium text-xs tracking-tight truncate">
-            Bio
-          </p>
-        </button>
+          <div className="group flex flex-col gap-2 items-start justify-center w-full h-auto">
+            <div className="flex flex-row gap-2 items-center justify-between w-full h-auto">
+              <p className="text-[#FFFFFF] font-medium text-sm tracking-tight truncate">
+                Bio
+              </p>
+              <Image
+                className="group opacity-0 group-hover:opacity-100"
+                src={icon.accountManagement}
+                width="16"
+                height="16"
+              />{" "}
+            </div>
+            <p className="text-[#FFFFFF] font-normal text-xs tracking-tight truncate">
+              Available
+            </p>
+          </div>
+        </div>
         <button className="bg-[#FFFFFF] transition-all hover:bg-[#FFFFFF]/80 px-4 py-4 rounded-lg flex flex-row gap-4 items-center justify-center w-auto lg:w-full h-auto mt-0">
           <p className="hidden lg:flex text-[#2563EB] font-medium text-sm tracking-tight">
             Keluar
